6dcaefec45aaa333ff89724e0c7af18d47e9d7be
“Jessica <emereony@ualberta.ca>
2023-01-31 20:04:45 -0700

    New class star

---
+++ app/src/main/java/com/example/qarmy/star.java
@@ -0,0 +1,12 @@
+package com.example.qarmy;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.shapes.Shape;
+
+public class star extends Shape {
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
+        
+    }
+}
