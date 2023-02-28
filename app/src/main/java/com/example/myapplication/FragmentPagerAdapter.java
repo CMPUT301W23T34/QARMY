12229a2d5a2bf4fd9cdbb12471755f13a2a0df7c
Japkirat Kaur <japkirat@ualberta.ca>
2023-02-28 14:05:21 -0700

    Added swipe navigation

---
+++ app/src/main/java/com/example/myapplication/FragmentPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentPagerAdapter extends FragmentStateAdapter {
+    public FragmentPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch(position){
+            case 0:
+                return new MapFragment();
+            case 1:
+                return new HomeFragment();
+            case 2:
+                return new RankFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
