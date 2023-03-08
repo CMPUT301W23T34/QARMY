909fc4fa20b97a606c67294856fc42761a17d91b
Japkirat Kaur <japkirat@ualberta.ca>
2023-03-07 18:24:34 -0700

    Added mapView

--- app/src/main/java/com/example/QArmy/MapFragment.java
+++ app/src/main/java/com/example/QArmy/MapFragment.java
@@ -1,18 +1,116 @@
 package com.example.QArmy;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-public class MapFragment extends Fragment {
-    public MapFragment(){
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+public class MapFragment extends Fragment {
+
+    private MapView mapView = null;
+    private IMapController mapController;
+    private MyLocationNewOverlay locationOverlay;
+
+    public MapFragment(){
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        Context context = getActivity().getApplicationContext();
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
+        requestPermissionsIfNecessary(context);
+
+        mapView = new MapView(getActivity());
+        ((LinearLayout) view.findViewById(R.id.map)).addView(mapView);
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+        mapView.setMultiTouchControls(true);
+
+        mapController = mapView.getController();
+
+        locationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context),mapView);
+        locationOverlay.enableMyLocation();
+        locationOverlay.enableFollowLocation();
+        locationOverlay.setDrawAccuracyEnabled(true);
+        mapController.animateTo(locationOverlay.getMyLocation());
+        mapController.setZoom(10.5);
+
+
+        mapView.getOverlays().add(this.locationOverlay);
+
+
+        mapView.setOnTouchListener((v, event) -> {
+            if(100 * event.getX() > 10 * v.getWidth() && 100 * event.getX() < 90 * v.getWidth()) {
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+            return true;
+        });
+
+        return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (mapView != null) {
+            mapView.onPause();
+        }
+        super.onPause();
+    }
+
+
+    private void requestPermissionsIfNecessary(Context context) {
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityResultLauncher<String[]> locationPermissionRequest =
+                    registerForActivityResult(new ActivityResultContracts
+                                    .RequestMultiplePermissions(), result -> {
+                                Boolean fineLocationGranted = result.getOrDefault(
+                                        android.Manifest.permission.ACCESS_FINE_LOCATION, false);
+                                Boolean coarseLocationGranted = result.getOrDefault(
+                                        android.Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                                if (coarseLocationGranted == null || !coarseLocationGranted || fineLocationGranted == null || !fineLocationGranted)  {
+                                    getActivity().getFragmentManager().popBackStack();
+                                }
+                            }
+                    );
+            locationPermissionRequest.launch(new String[] {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+    }
+
 }
