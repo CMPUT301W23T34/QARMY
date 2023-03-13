751b00047b0835761fc7489fa6d04da6e1480aef
Kai Luedemann <kluedema@ualberta.ca>
2023-03-12 22:49:46 -0600

    Added intent tests

---
+++ app/src/androidTest/java/com/example/QArmy/MainActivityTest.java
@@ -0,0 +1,48 @@
+package com.example.QArmy;
+
+
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.QArmy.UI.CaptureAct;
+import com.example.QArmy.UI.MainActivity;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.osmdroid.views.MapView;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    @Before
+    public void setUp() {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void testNavigation() {
+        solo.clickOnView(solo.getView(R.id.navigation_map));
+        assertTrue(solo.waitForView(MapView.class));
+        solo.clickOnView(solo.getView(R.id.navigation_rank));
+        assertTrue(solo.waitForView(R.id.rank_list));
+        solo.clickOnView(solo.getView(R.id.navigation_home));
+        assertTrue(solo.waitForView(R.id.qr_code_list));
+    }
+
+    @After
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
+
+
+}
