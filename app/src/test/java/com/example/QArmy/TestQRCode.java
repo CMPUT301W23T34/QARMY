9236a7ad5c522501bf7762adaceb33265291d584
Kai Luedemann <kluedema@ualberta.ca>
2023-03-12 21:34:37 -0600

    Fixed PR issues

--- app/src/test/java/com/example/QArmy/TestQRCode.java
+++ app/src/test/java/com/example/QArmy/TestQRCode.java
@@ -18,26 +18,26 @@ public class TestQRCode {
 
     @Test
     public void testGenerateHash() {
-        QRCode testQRCode = new QRCode("BFG5DGW54\n", new User(""), null, new Date());
+        QRCode testQRCode = new QRCode("BFG5DGW54\n", new User(), null, new Date());
         // The hash should be
         assertEquals("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6", testQRCode.getHash());
     }
     @Test
     public void testGenerateScore(){
-        QRCode testQRCode = new QRCode("BFG5DGW54\n", new User(""), null, new Date());
+        QRCode testQRCode = new QRCode("BFG5DGW54\n", new User(), null, new Date());
         // Based on the proposed scoring system, this QRCode should have a score of 111
         assertEquals(111,testQRCode.getScore());
     }
     @Test
     public void testGenerateName() {
-        QRCode testQRCode = new QRCode("BFG5DGW54\n", new User(""), null, new Date());
+        QRCode testQRCode = new QRCode("BFG5DGW54\n", new User(), null, new Date());
         // Based on the naming system and hash, this QRCode should have the following name
         String intendedName = "Captain CyanThiccNobleSalmon";
         assertEquals(intendedName,testQRCode.getName());
     }
     @Test
     public void testGenerateID() {
-        User testUser = new User("TestUsername");
+        User testUser = new User("TestUsername", "", "");
         QRCode testQRCode = new QRCode("BFG5DGW54\n", testUser, null, new Date());
         // Based on the hash and user, the QRCode should have the following ID:
         String codeHash = testQRCode.getHash();
