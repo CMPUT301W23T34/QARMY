3a3563729c13401c712ce8d2aa29a775248738d2
Kai Luedemann <kluedema@ualberta.ca>
2023-03-13 15:35:36 -0600

    Added QR code and comment tests

--- app/src/androidTest/java/com/example/QArmy/CommentsActivityTest.java
+++ app/src/androidTest/java/com/example/QArmy/CommentsActivityTest.java
@@ -2,14 +2,13 @@ package com.example.QArmy;
 
 
 import android.app.Activity;
-import android.content.Intent;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.QArmy.UI.CaptureAct;
 import com.example.QArmy.UI.MainActivity;
-import com.example.QArmy.UI.qrcodes.QRCodeVisualRepActivity;
+import com.example.QArmy.UI.qrcodes.CommentsActivity;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -20,23 +19,20 @@ import org.osmdroid.views.MapView;
 
 import static org.junit.Assert.*;
 
-public class QRCodeVisualRepActivityTest {
+public class CommentsActivityTest {
     private Solo solo;
 
     @Rule
-    public ActivityTestRule<QRCodeVisualRepActivity> rule = new ActivityTestRule<>(QRCodeVisualRepActivity.class, true, false);
+    public ActivityTestRule<CommentsActivity> rule = new ActivityTestRule<>(CommentsActivity.class, true, true);
 
     @Before
     public void setUp() {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
-        Intent intent = new Intent();
-        intent.putExtra("Object", "testname, testhash");
-        rule.launchActivity(intent);
     }
 
     @Test
     public void testNavigation() {
-        assertTrue(solo.waitForText("Lat"));
+        assertTrue(solo.waitForText("Only"));
     }
 
     @After
