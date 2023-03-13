8b779dbea57bd6e98096717b25ce5493fb1e4eb0
yghaznav <yghaznav@ualberta.ca>
2023-03-13 13:06:45 -0600

    Added some documentation to this UI

--- app/src/main/java/com/example/QArmy/UI/qrcodes/Constants.java
+++ app/src/main/java/com/example/QArmy/UI/qrcodes/Constants.java
@@ -2,14 +2,28 @@ package com.example.QArmy.UI.qrcodes;
 
 import com.example.QArmy.model.QRCode;
 
+/**
+ * This class provides a way to store and retrieve a single {@link QRCode} object
+ * The {@link QRCode} object can be set using the {@link Constants#setQrCode(QRCode)} method, and retrieved using
+ * the {@link Constants#getQrCode()} method.
+ * @author yasminghaznavian
+ */
 public class Constants {
 
     private static QRCode qrCode;
 
+    /**
+     * Returns the {@link QRCode} object that is stored in this class
+     */
     public static QRCode getQrCode() {
         return qrCode;
     }
 
+    /**
+     * Sets the {@link QRCode} object that is stored in this class.
+     *
+     * @param qrCode The {@link QRCode} object to store in this class.
+     */
     public static void setQrCode(QRCode qrCode) {
         Constants.qrCode = qrCode;
     }
