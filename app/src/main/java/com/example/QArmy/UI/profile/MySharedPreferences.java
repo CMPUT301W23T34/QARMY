c5fdd162907a9780996cc089bd5957bd02cc7b8a
Jessica E <122407139+jessicaeee@users.noreply.github.com>
2023-03-12 23:35:48 -0600

    Update MySharedPreferences.java

--- app/src/main/java/com/example/QArmy/UI/profile/MySharedPreferences.java
+++ app/src/main/java/com/example/QArmy/UI/profile/MySharedPreferences.java
@@ -7,7 +7,6 @@ import com.example.QArmy.model.User;
 
 public class MySharedPreferences {
     private static final String USER_PROFILE_PREFS = "user_profile_prefs";
-    private static final String QR_CODE_PREFS = "qr_code_prefs";
     private static final String DEVICE_ID_PREFERENCE = "device_id_prefs";
 
     public static void saveUserProfile(Context context, User user) {
