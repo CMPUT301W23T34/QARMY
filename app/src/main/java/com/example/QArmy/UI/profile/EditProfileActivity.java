9d031bd1cac11ea4797283626bab5b663db0fddd
Jessica E <122407139+jessicaeee@users.noreply.github.com>
2023-03-12 23:33:18 -0600

    Update EditProfileActivity.java

--- app/src/main/java/com/example/QArmy/UI/profile/EditProfileActivity.java
+++ app/src/main/java/com/example/QArmy/UI/profile/EditProfileActivity.java
@@ -1,8 +1,10 @@
 package com.example.QArmy.UI.profile;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +14,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.QArmy.R;
+import com.example.QArmy.UI.MainActivity;
 import com.example.QArmy.model.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditProfileActivity extends AppCompatActivity {
     private DatabaseReference db_reference;
@@ -34,12 +38,16 @@ public class EditProfileActivity extends AppCompatActivity {
     private String email;
     private String phone;
 
+    // SharedPreferences
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        //get user info
+        // Retrieve user information
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
         email = intent.getStringExtra("email");
@@ -50,6 +58,7 @@ public class EditProfileActivity extends AppCompatActivity {
         edit_email = findViewById(R.id.edit_email);
         edit_phone = findViewById(R.id.edit_phone);
         save_button = findViewById(R.id.save_button);
+        home_button1 = findViewById(R.id.home_button1);
 
         // Display user information in UI elements
         edit_name.setText(name);
@@ -60,6 +69,8 @@ public class EditProfileActivity extends AppCompatActivity {
         save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("EditProfileActivity", "Edit button clicked");
+
                 // Get updated user info from UI elements
                 name = edit_name.getText().toString().trim();
                 email = edit_email.getText().toString().trim();
@@ -81,15 +92,18 @@ public class EditProfileActivity extends AppCompatActivity {
                     return;
                 }
 
-                // Update user info in Database
+                // Update user info in  Database
                 User updatedUser = new User(name, email, phone);
-                db_reference.child("Players").child(current_user.getUid()).setValue(updatedUser)
+                DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Players");
+                dbRef.child(current_user.getUid()).setValue(updatedUser)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
                                 Toast.makeText(EditProfileActivity.this, "User info updated", Toast.LENGTH_SHORT).show();
-                                // go back to profile page
                                 Intent intent = new Intent(EditProfileActivity.this, UserProfileActivity.class);
+                                intent.putExtra("name", name);
+                                intent.putExtra("email", email);
+                                intent.putExtra("phone", phone);
                                 startActivity(intent);
                                 finish();
                             }
@@ -98,24 +112,39 @@ public class EditProfileActivity extends AppCompatActivity {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Toast.makeText(EditProfileActivity.this, "Failed to update user info", Toast.LENGTH_SHORT).show();
-                                // go back to profile page
-                                Intent intent = new Intent(EditProfileActivity.this, UserProfileActivity.class);
-                                startActivity(intent);
-                                finish();
                             }
                         });
             }
         });
-        // Set click listener for Home button
+
+        // if home button is clicked, go back to homepage
         home_button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EditProfileActivity.this, RegistrationActivity.class);
+                Intent intent = new Intent(EditProfileActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (db_reference == null) {
+            db_reference = FirebaseDatabase.getInstance().getReference();
+        }
+
+        // Retrieve updated user information
+        Intent intent = getIntent();
+        String updatedName = intent.getStringExtra("name");
+        String updatedEmail = intent.getStringExtra("email");
+        String updatedPhone = intent.getStringExtra("phone");
+
+        // Update user information
+        edit_name.setText(updatedName);
+        edit_email.setText(updatedEmail);
+        edit_phone.setText(updatedPhone);
+
+    }
+
 }
-
-
-
