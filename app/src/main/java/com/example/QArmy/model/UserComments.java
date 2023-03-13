93a48ba47c5efd05888a7a0afd2c79a9a507d69f
Brett Merkosky <bamerkos@ualberta.ca>
2023-03-13 11:35:05 -0600

    Added documentation for model classes and tests for UserComments class

--- app/src/main/java/com/example/QArmy/model/UserComments.java
+++ app/src/main/java/com/example/QArmy/model/UserComments.java
@@ -1,5 +1,10 @@
 package com.example.QArmy.model;
 
+/**
+ * Represent a comment
+ * Contains username and message of the comment
+ * @author Yasmin Ghaznav
+ */
 public class UserComments {
 
     public UserComments(String username, String textMessage) {
@@ -10,18 +15,34 @@ public class UserComments {
     private String username;
     private String textMessage;
 
+    /**
+     * Get the username of the person who made a comment
+     * @return The user who made the comment
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Set the username of a person who made a comment
+     * @param username The new username of the person who wrote the comment
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Get the text of a comment
+     * @return The text of the comment
+     */
     public String getTextMessage() {
         return textMessage;
     }
 
+    /**
+     * Set the text of a comment
+     * @param textMessage The text the comment should be set to
+     */
     public void setTextMessage(String textMessage) {
         this.textMessage = textMessage;
     }
