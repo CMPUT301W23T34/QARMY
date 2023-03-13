a2d12aaee5fbbd6e93f906effc22795e85ebe2a7
Jessica E <122407139+jessicaeee@users.noreply.github.com>
2023-03-13 13:28:34 -0600

    update and clean up

--- app/src/main/java/com/example/QArmy/UI/profile/UserProfileActivity.java
+++ app/src/main/java/com/example/QArmy/UI/profile/UserProfileActivity.java
@@ -8,84 +8,68 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.QArmy.QArmy;
 import com.example.QArmy.R;
-import com.example.QArmy.UI.MainActivity;
 import com.example.QArmy.model.User;
 
-public class UserProfileActivity extends AppCompatActivity {
 
-    // UI elements
+// UserProfileActivity.java
+public class UserProfileActivity extends AppCompatActivity {
+    // UI Elements
     private TextView text_name;
     private TextView text_email;
     private TextView text_phone;
     private Button edit_button;
-    private Button home_button;
 
-    // User info
+    // User Info
     private String name;
     private String email;
     private String phone;
-    private String deviceId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        Intent intent = getIntent();
-        name = intent.getStringExtra("name");
-        email = intent.getStringExtra("email");
-        phone = intent.getStringExtra("phone");
-        deviceId = intent.getStringExtra("id");
-
-        String score = "";
-        User user = new User(name, email, phone, score, deviceId);
-
         // Initialize UI Elements
         text_name = findViewById(R.id.text_name);
         text_email = findViewById(R.id.text_email);
         text_phone = findViewById(R.id.text_phone);
         edit_button = findViewById(R.id.edit_button);
-        home_button = findViewById(R.id.home_button);
 
-        text_name.setText(name);
-        text_email.setText(email);
-        text_phone.setText(phone);
+        updateValues();
 
+        // Set click listener for Edit button
         edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create an Intent to start EditProfileActivity
                 Intent intent = new Intent(UserProfileActivity.this, EditProfileActivity.class);
+
                 // Pass the current user information to EditProfileActivity
                 intent.putExtra("name", name);
                 intent.putExtra("email", email);
                 intent.putExtra("phone", phone);
-                startActivity(intent);
-            }
-        });
-        home_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
-                startActivity(intent);
 
+                startActivity(intent);
             }
         });
     }
+
+    public void updateValues() {
+        User user = ((QArmy) getApplication()).getUser();
+        name = user.getName();
+        email = user.getEmail();
+        phone = user.getPhone();
+
+        text_name.setText(name);
+        text_email.setText(email);
+        text_phone.setText(phone);
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            // Get the updated user information
-            name = data.getStringExtra("name");
-            email = data.getStringExtra("email");
-            phone = data.getStringExtra("phone");
-
-            // Update new user information
-            text_name.setText(name);
-            text_email.setText(email);
-            text_phone.setText(phone);
-
-        }
+    protected void onResume() {
+        super.onResume();
+        updateValues();
     }
 }
