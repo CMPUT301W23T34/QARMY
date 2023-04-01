932305835812ac8133e7d411a0e4632c8b343eee
Jessica E <122407139+jessicaeee@users.noreply.github.com>
2023-04-01 17:23:53 -0600

    update profile

--- app/src/main/java/com/example/QArmy/UI/profile/RegistrationActivity.java
+++ app/src/main/java/com/example/QArmy/UI/profile/RegistrationActivity.java
@@ -1,151 +1,147 @@
-package com.example.QArmy.UI.profile;
+    package com.example.QArmy.UI.profile;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+    import android.annotation.SuppressLint;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.provider.Settings;
+    import android.text.TextUtils;
+    import android.util.Log;
+    import android.util.Patterns;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.Toast;
+    import androidx.annotation.NonNull;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.QArmy.QArmy;
-import com.example.QArmy.R;
-import com.example.QArmy.model.User;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+    import com.example.QArmy.QArmy;
+    import com.example.QArmy.R;
+    import com.example.QArmy.model.User;
+    import com.google.android.gms.tasks.OnFailureListener;
+    import com.google.android.gms.tasks.OnSuccessListener;
 
-import com.google.firebase.firestore.FirebaseFirestore;
+    import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Represents the registration page for new users
- * @author Jessica Emereonye
- */
-public class RegistrationActivity extends AppCompatActivity {
-    private EditText email_or_phone;
-    private EditText username;
-    private EditText password;
-    private Button register_button;
-    private FirebaseFirestore db;
+    import java.util.HashMap;
+    import java.util.Map;
 
     /**
-     * Checks whether a new email is valid
-     * @param target The new email the user is attempting to add
-     * @return Whether or not this email already exists in the database
+     * Represents the registration page for new users
+     * @author Jessica Emereonye
      */
-    public static boolean isValidEmail(CharSequence target) {
-        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
-    }
+    public class RegistrationActivity extends AppCompatActivity {
+        private EditText email_or_phone;
+        private EditText username;
+        private Button register_button;
+        private FirebaseFirestore db;
 
-    /**
-     * Checks whether a new phone number is valid
-     * @param target The new phone number the user is attempting to add
-     * @return Whether or not this phone number already exists in the database
-     */
-    public static boolean isValidPhoneNumber(CharSequence target) {
-        return (!TextUtils.isEmpty(target) && Patterns.PHONE.matcher(target).matches());
-    }
+        /**
+         * Checks whether a new email is valid
+         * @param target The new email the user is attempting to add
+         * @return Whether or not this email already exists in the database
+         */
+        public static boolean isValidEmail(CharSequence target) {
+            return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+        }
 
-    /**
-     * Initialize the activity
-     * @param savedInstanceState
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        /**
+         * Checks whether a new phone number is valid
+         * @param target The new phone number the user is attempting to add
+         * @return Whether or not this phone number already exists in the database
+         */
+        public static boolean isValidPhoneNumber(CharSequence target) {
+            return (!TextUtils.isEmpty(target) && Patterns.PHONE.matcher(target).matches());
+        }
 
-        db = FirebaseFirestore.getInstance();
+        /**
+         * Initialize the activity
+         * @param savedInstanceState
+         */
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_registration);
 
-        // Get device id
-        @SuppressLint("HardwareIds") String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-        email_or_phone = findViewById(R.id.email_or_phone);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        register_button = findViewById(R.id.register_button);
+            db = FirebaseFirestore.getInstance();
 
-        // Set OnClickListener for Register button
-        register_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Get user input
-                String email_phoneInput = RegistrationActivity.this.email_or_phone.getText().toString().trim();
-                String usernameInput = RegistrationActivity.this.username.getText().toString().trim();
-                String passwordInput = RegistrationActivity.this.password.getText().toString().trim();
+            // Get device id
+            @SuppressLint("HardwareIds") String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+            email_or_phone = findViewById(R.id.email_or_phone);
+            username = findViewById(R.id.username);
+            register_button = findViewById(R.id.register_button);
 
-                // Validate user input
-                if (TextUtils.isEmpty(email_phoneInput)) {
-                    Toast.makeText(getApplicationContext(), "Please enter your email or phone number:", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+            // Set OnClickListener for Register button
+            register_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Get user input
+                    String email_phoneInput = RegistrationActivity.this.email_or_phone.getText().toString().trim();
+                    String usernameInput = RegistrationActivity.this.username.getText().toString().trim();
 
-                if (!isValidEmail(email_phoneInput) && !isValidPhoneNumber(email_phoneInput)) {
-                    Toast.makeText(getApplicationContext(), "Please enter a valid email or phone number:", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                    // Validate user input
+                    if (TextUtils.isEmpty(email_phoneInput)) {
+                        Toast.makeText(getApplicationContext(), "Please enter your email or phone number:", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
 
-                if (TextUtils.isEmpty(usernameInput)) {
-                    Toast.makeText(getApplicationContext(), "Please choose a username:", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                    if (!isValidEmail(email_phoneInput) && !isValidPhoneNumber(email_phoneInput)) {
+                        Toast.makeText(getApplicationContext(), "Please enter a valid email or phone number:", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
 
-                if (TextUtils.isEmpty(passwordInput)) {
-                    Toast.makeText(getApplicationContext(), "Please enter a password:", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                    //check if username is taken
-                    db.collection("Players").document(usernameInput)
-                            .get()
-                            .addOnSuccessListener(documentSnapshot -> {
-                                if (documentSnapshot.exists()) {
-                                    Toast.makeText(getApplicationContext(), "Username already taken. Please choose another one.", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Map<String, Object> userObject = new HashMap<>();
-                                    userObject.put("email", email_phoneInput);
-                                    userObject.put("userName", usernameInput);
-                                    userObject.put("password", passwordInput);
-                                    userObject.put("deviceID", deviceID);
-                                    ((QArmy) getApplication()).setUser(new User(usernameInput, email_phoneInput, "", "100", deviceID));
-
+                    if (TextUtils.isEmpty(usernameInput)) {
+                        Toast.makeText(getApplicationContext(), "Please choose a username:", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                                    //check if username is taken
                                     db.collection("Players").document(usernameInput)
-                                            .set(userObject)
-                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                @Override
-                                                public void onSuccess(Void aVoid) {
-                                                    Log.e("RegistrationActivity", "DocumentSnapshot successfully written!");
-
-                                                    // save created account to shared preferences
+                                            .get()
+                                            .addOnSuccessListener(documentSnapshot -> {
+                                                if (documentSnapshot.exists()) {
+                                                    Toast.makeText(getApplicationContext(), "Username already taken. Please choose another one.", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Map<String, Object> userObject = new HashMap<>();
+                                                    userObject.put("email", email_phoneInput);
+                                                    userObject.put("userName", usernameInput);
+                                                    userObject.put("deviceID", deviceID);
                                                     ((QArmy) getApplication()).setUser(new User(usernameInput, email_phoneInput, "", "100", deviceID));
-                                                    MySharedPreferences.saveUserProfile(getApplicationContext(), new User(
-                                                            usernameInput,
-                                                            email_phoneInput,
-                                                            "",
-                                                            "100",
-                                                            deviceID
-                                                    ));
-                                                    finish();
 
-                                                }
-                                            })
-                                            .addOnFailureListener(new OnFailureListener() {
-                                                @Override
-                                                public void onFailure(@NonNull Exception e) {
-                                                    Log.e("RegistrationActivity", "Error writing document", e);
+                                                    db.collection("Players").document(usernameInput)
+                                                            .set(userObject)
+                                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                @Override
+                                                                public void onSuccess(Void aVoid) {
+                                                                    Log.e("RegistrationActivity", "DocumentSnapshot successfully written!");
+                                                                    // save created account to shared preferences
+                                                                    ((QArmy) getApplication()).setUser(new User(usernameInput, email_phoneInput, "", "100", deviceID));
+                                                                    MySharedPreferences.saveUserProfile(getApplicationContext(), new User(
+                                                                            usernameInput,
+                                                                            email_phoneInput,
+                                                                            "",
+                                                                            "100",
+                                                                            deviceID
+                                                                    ));
+                                                                    Intent intent = new Intent(RegistrationActivity.this, UserProfileActivity.class);
+                                                                    intent.putExtra("name", usernameInput);
+                                                                    intent.putExtra("email", email_phoneInput);
+                                                                    startActivity(intent);
+                                                                    finish();
+
+                                                                }
+                                                            })
+                                                            .addOnFailureListener(new OnFailureListener() {
+                                                                @Override
+                                                                public void onFailure(@NonNull Exception e) {
+                                                                    Log.e("RegistrationActivity", "Error writing document", e);
+                                                                }
+                                                            });
                                                 }
                                             });
-                                }
-                            });
+                }
+                });
+
 
             }
-            });
+    }
 
 
-        }
-}
