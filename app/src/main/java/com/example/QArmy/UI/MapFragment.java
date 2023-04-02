8665a0e7246d30a7bde3b6cd88648797ec14da85
Brett Merkosky <bamerkos@ualberta.ca>
2023-04-02 17:38:32 -0600

    Fixed how the map fragment sends QRCodes to the Visual Rep Activity

--- app/src/main/java/com/example/QArmy/UI/MapFragment.java
+++ app/src/main/java/com/example/QArmy/UI/MapFragment.java
@@ -145,9 +145,9 @@ public class MapFragment extends Fragment {
                     @Override
                     public boolean onItemSingleTapUp(final int index,
                                                      final OverlayItem item) {
-                        String selectedQR = qrLocationList.getQrCodes().get(index).getName()+","+qrLocationList.getQrCodes().get(index).getScore();
+                        QRCode selectedQR = qrLocationList.getQrCodes().get(index);
                         Intent intent = new Intent(getContext(), QRCodeVisualRepActivity.class);
-                        intent.putExtra("Object",selectedQR);
+                        intent.putExtra("QRCode",selectedQR);
                         startActivity(intent);
 
                         return true;
