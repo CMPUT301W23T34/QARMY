f90b57948d66714816c2da926f8b3339986545c4
Japkirat Kaur <japkirat@ualberta.ca>
2023-04-02 21:55:16 -0600

    added test for QRLocationList and fixed null error

--- app/src/main/java/com/example/QArmy/UI/QRLocationList.java
+++ app/src/main/java/com/example/QArmy/UI/QRLocationList.java
@@ -10,36 +10,61 @@ import org.osmdroid.views.overlay.OverlayItem;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Model class storing arraylist of QRCodes and corresponding OverlayItems for the map
+ * @author Japkirat Kaur
+ */
 public class QRLocationList extends TModel<TView> {
     private ArrayList<QRCode> qrCodes;
     private ArrayList<OverlayItem> itemList;
 
+    /**
+     * Initialize empty arrayLists
+     */
     public QRLocationList () {
         qrCodes = new ArrayList<>();
         itemList = new ArrayList<>();
     }
 
+    /**
+     * Resets qr and overlay item lists
+     * @param newCodes
+     */
     public void modify(List<QRCode> newCodes) {
         qrCodes.clear();
-        qrCodes.addAll(newCodes);
+        for (QRCode qr : newCodes) {
+            if (qr.getLat() != null || qr.getLon() != null) {
+                qrCodes.add(qr);
+            }
+        }
         updateQrLocations();
         notifyViews();
     }
 
+    /**
+     * Updates list of OverlayItems
+     */
     private void updateQrLocations() {
         itemList.clear();
         for (QRCode qr : qrCodes) {
-            if (qr.getLat() != null || qr.getLon() != null) {
-                GeoPoint g = new GeoPoint(qr.getLat(), qr.getLon());
-                itemList.add(new OverlayItem(qr.getHash(), qr.getName(), g));
-            }
+            GeoPoint g = new GeoPoint(qr.getLat(), qr.getLon());
+            itemList.add(new OverlayItem(qr.getHash(), qr.getName(), g));
+
         }
     }
 
+    /**
+     * Get the list of QRCodes
+     * @return the ArrayList of QRCodes
+     */
     public ArrayList<QRCode> getQrCodes() {
         return qrCodes;
     }
 
+    /**
+     * Get the list of OverlayItems
+     * @return the ArrayList of OverlayItems
+     */
     public ArrayList<OverlayItem> getItemList() {
         return itemList;
     }
--- app/src/test/java/com/example/QArmy/TestQRLocationList.java
+++ app/src/test/java/com/example/QArmy/TestQRLocationList.java
@@ -1,4 +1,74 @@
 package com.example.QArmy;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.location.Location;
+import android.location.LocationManager;
+
+import com.example.QArmy.UI.QRLocationList;
+import com.example.QArmy.model.QRCode;
+import com.example.QArmy.model.User;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+/**
+ * Provides tests for QRLocationList  class
+ * @author Japkirat Kaur
+ */
 public class TestQRLocationList {
+
+    /**
+     * Create a mock QR code
+     * @return Empty QR code
+     */
+    public QRCode MockEmptyCode() {
+        return new QRCode();
+    }
+
+    /**
+     * Create a mock QR code
+     * @return  QRCode
+     */
+    public QRCode MockCode() {
+        return new QRCode("http://en.m.wikipedia.org",new User("test"),new Location(LocationManager.NETWORK_PROVIDER),new Date());
+    }
+
+    /**
+     * Create a mock QR list
+     * @return Empty QR list
+     */
+    public QRLocationList MockQRLocationList() {
+        return new QRLocationList();
+    }
+
+    /**
+     * Test the modify method
+     */
+    @Test
+    public void testModify() {
+        QRLocationList list = MockQRLocationList();
+        ArrayList<QRCode> codes = new ArrayList<QRCode>();
+
+        codes.add(MockEmptyCode());
+        list.modify(codes);
+        assertEquals(0, list.getQrCodes().size());
+        assertEquals(0, list.getItemList().size());
+
+        codes.add(MockCode());
+        list.modify(codes);
+        assertEquals(1, list.getQrCodes().size());
+        assertEquals(1, list.getItemList().size());
+        assertTrue(list.getQrCodes().contains(codes.get(1)));
+
+        codes.clear();
+        list.modify(codes);
+        assertEquals(0, list.getQrCodes().size());
+        assertEquals(0, list.getItemList().size());
+    }
+
 }
